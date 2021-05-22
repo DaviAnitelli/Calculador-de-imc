b443473beba5368ffe36800b39f94262f829cc69
DaviAnitelli <davianitelli272@hotmail.com>
2021-05-22 12:42:34 -0300

    Calculadora de IMC

--- App.js
+++ App.js
@@ -1,12 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { 
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity
+} from 'react-native';
 
-export default function App() {
-  return (
+export default function App(){
+  const [peso, setPeso] = useState('');
+  const [altura, setAltura] = useState('');
+
+  function handleSubmit(){
+    const alt = altura / 100;
+    const imc = peso / (alt * alt);
+    
+    if(imc < 18.6){
+      alert('Você está abaixo do peso! \nIMC:' + imc.toFixed(2));
+    }else if(imc >= 18.6 && imc < 24.9){
+      alert('Peso ideal! \nIMC:' + imc.toFixed(2));
+    }else if(imc >= 24.9 && imc < 34.9){
+      alert('Levemente acima do peso! \nIMC:' + imc.toFixed(2));
+    }else if(imc >= 34.9){
+      alert('Obeso! \nIMC:' + imc.toFixed(2));
+    }
+  }
+
+  return(
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>
+        Calcule o seu IMC
+      </Text>
+
+      <TextInput
+        style={styles.input}
+        value={peso}
+        onChangeText={ (peso) => setPeso(peso) }
+        placeholder="Peso(Kg)"
+        keyboardType="numeric"
+      />
+
+      <TextInput
+        style={styles.input}
+        value={altura}
+        onChangeText={ (altura) => setAltura(altura) }
+        placeholder="Altura"
+        keyboardType="numeric"
+      />
+
+      <TouchableOpacity 
+        style={styles.button}
+        onPress={handleSubmit}
+      >
+        <Text style={styles.buttonText}>
+          Calcular
+        </Text>
+      </TouchableOpacity>
+
     </View>
   );
 }
@@ -14,8 +64,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  title: {
+    textAlign: 'center',
+    marginTop: 25,
+    fontSize: 30
+  },
+  input:{
+    backgroundColor: '#1212',
+    borderRadius: 10,
+    margin: 15,
+    padding: 10,
+    color: '#121212',
+    fontSize: 23
+  },
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 15,
+    backgroundColor: '#41aef4',
+    padding: 10,
+    borderRadius: 40
+  },
+  buttonText:{
+    color: '#FFF',
+    fontSize: 25
+  }
 });
